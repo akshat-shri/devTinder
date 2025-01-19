eee1d6eee9c1efea881d0943ea5c4e2830825fff
akshat-shri <akshatshrivastava702@gmail.com>
2025-01-19 13:55:21 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,37 +2,9 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/h",(req,res) => {                  //Request handler
-//     res.send("Hello developer!  ");
-// });
-
-// app.use("/test1",(req,res) => {                 //Request handler
-//     res.send("Akshat Shrivastava!");
-// });
-
-// app.use("/user",(req,res) => {                  //Request handler
-//     res.send("Hello from Akshat Shrivastava!");
-// });
-
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({Firstname:"Akshat", Lastname:"Shrivastava"}); 
-});
-
-app.post("/user",(req,res)=>{
-    // data save in databse
-    res.send("Data successully saved to database");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("data successfully deleted");
-});
-
-app.patch("/user",(req,res)=>{
-    res.send("data successfully updated");
-});
-
-app.put("/user",(req,res)=>{
-    res.send("data successfully replaced");
 });
 
 app.listen(2000,()=>{
