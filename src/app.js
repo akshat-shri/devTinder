4b60e2d410170da2f6a156de3f195f45eb468714
akshat-shri <akshatshrivastava702@gmail.com>
2025-01-19 13:32:43 +0530

    exploring CRUD

--- src/app.js
+++ src/app.js
@@ -2,16 +2,37 @@ const express = require("express");
 
 const app = express();
 
-app.use("/here",(req,res) => {                  //Request handler
-    res.send("Hello!");
+// app.use("/h",(req,res) => {                  //Request handler
+//     res.send("Hello developer!  ");
+// });
+
+// app.use("/test1",(req,res) => {                 //Request handler
+//     res.send("Akshat Shrivastava!");
+// });
+
+// app.use("/user",(req,res) => {                  //Request handler
+//     res.send("Hello from Akshat Shrivastava!");
+// });
+
+app.get("/user",(req,res)=>{
+    res.send({Firstname:"Akshat", Lastname:"Shrivastava"});
 });
 
-app.use("/test1",(req,res) => {                 //Request handler
-    res.send("Akshat Shrivastava!");
+app.post("/user",(req,res)=>{
+    // data save in databse
+    res.send("Data successully saved to database");
 });
 
-app.use("/test",(req,res) => {                  //Request handler
-    res.send("Hello from Akshat Shrivastava!");
+app.delete("/user",(req,res)=>{
+    res.send("data successfully deleted");
+});
+
+app.patch("/user",(req,res)=>{
+    res.send("data successfully updated");
+});
+
+app.put("/user",(req,res)=>{
+    res.send("data successfully replaced");
 });
 
 app.listen(2000,()=>{
