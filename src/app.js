d7d01bbbf8dcbe445b731627b15390a98e385f14
akshat-shri <akshatshrivastava702@gmail.com>
2025-01-19 16:45:16 +0530

    writing middleware for auth

--- src/app.js
+++ src/app.js
@@ -1,31 +1,21 @@
 const express = require("express");
+const {adminAuth, userAuth} = require("./middlewares/auth")
 
 const app = express();
 
-app.get(
-  "/user",
-  
-  (req, res, next) => {
-    console.log("handle first route");
-    next();
-    // res.send("response");
-  },
-  (req, res, next) => {
-    console.log("handle second route");
-    // res.send("2nd response");
-    next();
-  },
-  (req, res,next) => {
-    console.log("handle third route")
-    // res.send("3rd response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("handle forth route");
-    // res.send("4th response");
-  }
+app.use("/admin",adminAuth);
 
-);
+app.use("/user",userAuth,(req,res)=>{
+    res.send("user authorized");
+});
+
+app.get("/admin/getAllData",(req,res)=>{
+        res.send("yup get it");
+});
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("user deleted");
+})
 
 app.listen(2000, () => {
   console.log("Server is running on port 2000");
---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+const adminAuth = (req,res,next)=>{
+    const token = "xyz";
+    const isAuthorized = token ==="xyz";
+    if(!isAuthorized){
+        res.status(401).send("unauthorized");
+    }
+    else{
+    next();
+    }
+};
+
+const userAuth = (req,res,next)=>{
+    const token = "xyz";
+    const isAuthorized = token ==="yz";
+    if(!isAuthorized){
+        res.status(401).send("unauthorized");
+    }
+    else{
+    next();
+    }
+};
+
+module.exports = {
+     adminAuth,
+     userAuth
+}
