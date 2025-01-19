75413c14a5e7fb4c4ae925b8edd98dd1399673b4
akshat-shri <akshatshrivastava702@gmail.com>
2025-01-19 15:48:02 +0530

    multiple routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({Firstname:"Akshat", Lastname:"Shrivastava"}); 
-});
+app.get(
+  "/user",
+  
+  (req, res, next) => {
+    console.log("handle first route");
+    next();
+    // res.send("response");
+  },
+  (req, res, next) => {
+    console.log("handle second route");
+    // res.send("2nd response");
+    next();
+  },
+  (req, res,next) => {
+    console.log("handle third route")
+    // res.send("3rd response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handle forth route");
+    // res.send("4th response");
+  }
 
-app.listen(2000,()=>{
-    console.log("Server is running on port 2000");                                                                  
+);
+
+app.listen(2000, () => {
+  console.log("Server is running on port 2000");
 });
