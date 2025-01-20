9ed2c7804f28e9923e444c65823976163e43981f
akshat-shri <akshatshrivastava702@gmail.com>
2025-01-20 09:41:44 +0530

    dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -3,32 +3,25 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async(req,res)=>{
-    const user = new User({
-        firstName: "Akshat",
-        lastName:"Shrivastava",
-        emailId:"akshatshri@gmail.com",
-        password:"akshat@",
-    });
+app.use(express.json());
 
-    try{
-        await user.save();
-        res.send("User created");
-    } catch(err){
-        res.status(400).send("errpr in creating user:"+ err.message);
-    }
-
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User created");
+  } catch (err) {
+    res.status(400).send("error in creating user:" + err.message);
+  }
 });
 
-
- connectDB()
+connectDB()
   .then(() => {
     console.log("Database connected");
     app.listen(2000, () => {
-        console.log("Server is running on port 2000");
-      });
+      console.log("Server is running on port 2000");
+    });
   })
   .catch((err) => {
     console.log("error in connecting database");
   });
-
