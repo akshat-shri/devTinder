298f7fbad1ee7151844ace53587000a10c8104a2
akshat-shri <akshatshrivastava702@gmail.com>
2025-03-01 12:21:13 +0530

    Update password Api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -25,6 +27,22 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       message: `${loggedInUser.firstName}, your profile updated successfully`,
       data: loggedInUser,
     });
+  } catch (err) {
+    res.status(400).send("ERROR:" + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { newPassword, confirmNewPassword } = req.body;
+    if (newPassword !== confirmNewPassword) {
+      throw new Error("new password do not match");
+    }
+    const user = req.user;
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+    user.password = passwordHash;
+    await user.save();
+    res.send("passsword updated successfully");
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
   }
