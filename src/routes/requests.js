d1dfc6e603ae3cef079b13d33aec00d10dbca942
akshat-shri <akshatshrivastava702@gmail.com>
2025-03-06 20:51:59 +0530

    build connectionRequest Accept and reject API

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -45,9 +45,44 @@ requestRouter.post(
 
       const data = await connectionRequest.save();
       res.json({
-        message: req.user.firstName + " is " + status + " in "+ toUser.firstName,
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
         data,
       });
+    } catch (err) {
+      res.status(400).send("ERROR:" + err.message);
+    }
+  }
+);
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Status not allowed"});
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .send({ message: "connection request not found" });
+      }
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+      res.json({ message: "connection request " + status, data });
     } catch (err) {
       res.status(400).send("ERROR:" + err.message);
     }
