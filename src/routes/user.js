b4d69492fc0ca7d2d89b71eba4c83f30f2771130
akshat-shri <akshatshrivastava702@gmail.com>
2025-03-07 17:44:28 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,6 +53,11 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit =  limit > 50 ? 50 : limit;
+    const skip = (page-1) * limit;
+
     const connectionRequest = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
@@ -68,7 +73,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);;
     res.send(users);
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
@@ -76,3 +81,6 @@ userRouter.get("/feed", userAuth, async (req, res) => {
 });
 
 module.exports = userRouter;
+
+// feed:skip -> this is params
+// feed:? -> this is query
